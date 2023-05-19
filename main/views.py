fb5ac94124d6fe30c8c74a8eabb5c2c0875dfb80
your_github_username <jakub.galuszka@interia.pl>
2023-05-19 23:12:40 +0200

    working on database

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import TypesList, Type
 # Create your views here.
 
-def index(response):
-    return HttpResponse("Welcome to Lotto checker site")
+def index(response, id):
+    ls = TypesList.objects.get(id=id)
+    typ = ls.type_set.get()
+    return HttpResponse("Welcome to Lotto checker site %s %s" % (ls.name, typ.typing))
 
-def v1(response):
-    return HttpResponse("Welcome to Lotver site")
+
