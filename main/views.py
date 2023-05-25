9fcfe8ccf00292f1a4e7b970a4a8a35c81c17269
your_github_username <jakub.galuszka@interia.pl>
2023-05-25 23:46:44 +0200

    New forms added

--- main/views.py
+++ main/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import TypesList, Type
 from .forms import CreateNewType
 # Create your views here.
 
 def index(response, id):
     ls = TypesList.objects.get(id=id)
+    
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for type in ls.type_set.all():
+                print(response.POST.get("c" + str(type.id)))
+                if response.POST.get("c" + str(type.id)) == "clicked":
+                    type.winner=True
+                else:
+                    type.winner=False
+                type.save()
+        elif response.POST.get("newType"):
+            txt = response.POST.get("new")
+            if len(txt) > 2:
+                ls.type_set.create(typing=txt, winner=False)
+            else:
+                print("invalid input")
+            
+
     return render(response, "main/type.html", {"ls":ls})
 
 def list(response):
@@ -16,5 +35,15 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def create(response):
-    form = CreateNewType()
-    return render(response, "main/create.html", {"form":form})
+    if response.method == "POST":
+        form = CreateNewType(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = TypesList(name=n)
+            t.save() 
+
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewType()
+        return render(response, "main/create.html", {"form":form})
