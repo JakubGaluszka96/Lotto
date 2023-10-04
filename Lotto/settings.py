ad2c7d02c2234ea3f32d159d12a5451660eb152b
your_github_username <jakub.galuszka@interia.pl>
2023-10-04 10:07:49 +0200

    Working admin

--- Lotto/settings.py
+++ Lotto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-w--pems-eyrbbvz8-9o9@q+es!w=$=5txw!459*h=7iht33!r8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jakub96.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -133,3 +136,4 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL= "/"
 LOGOUT_REDIRECT_URL= "/"
+
